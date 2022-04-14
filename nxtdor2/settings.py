164d2625cc2a130da30a5c5674e6a9e6a69ea10d
IssamHoss <ize02@mail.aub.edu>
2022-04-14 15:29:24 +0300

    added static files

--- nxtdor2/settings.py
+++ nxtdor2/settings.py
@@ -41,10 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
