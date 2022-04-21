3831433f4ecff224e8d8e26d40600e7246f3b651
IssamHoss <ize02@mail.aub.edu>
2022-04-21 14:27:21 +0300

    attempt to fix

--- nxtdor2/settings.py
+++ nxtdor2/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'corsheaders',
+    'corsheaders'
 
 ]
 
@@ -130,8 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_ROOT= 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
